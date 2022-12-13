f245ac04b0009cb025859d591e80960afd81ec21
laxman-boda <laxman.boda96.com>
2022-12-13 13:03:12 +0530

    firebase setup is done

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Lets build a linkedin clone</h1>
+
+    {/* Header */}
+    {/* AppBody */}
+    {/* Sidebar */}
+    {/* Feed */}
+    {/* Widgets */}
+    </div>
+  );
+}
+
+export default App;
